b2aa11d76723839a39fe28ebe49adb5040572972
Chee Yung <cheeyung12@gmail.com>
2024-04-11 21:41:44 +0800

    Initial commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaBug } from "react-icons/fa";
+
+const Navbar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex space-x-6 border-b-2 mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`text-gray-400 hover:text-gray-700 ${
+              pathname === link.href ? "text-gray-900" : ""
+            }`}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
