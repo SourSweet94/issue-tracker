a71e5a531ec934cea45adfb55af293d70cbf1d8d
Chee Yung <cheeyung12@gmail.com>
2024-04-12 14:56:33 +0800

    Handle error

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-});
-
-// interface body {
-//   title: string;
-//   description: string;
-// }
+import { createIssueSchema } from "../../validation";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,20 +2,27 @@
 
 import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
-import Mde from "./Mde";
+// import Mde from "./Mde";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMdeReact from "react-simplemde-editor";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validation";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState<string>("");
   const router = useRouter();
 
@@ -33,6 +40,9 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Callout.Text color="red">{errors.title.message}</Callout.Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -40,6 +50,9 @@ const NewIssuePage = () => {
             <SimpleMdeReact placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Callout.Text color="red">{errors.description.message}</Callout.Text>
+        )}
 
         <Button>Submit new issue</Button>
       </form>
